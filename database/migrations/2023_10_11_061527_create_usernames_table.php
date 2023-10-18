55637fb8f9399000f3e9a6687c14810dfde6d4bd
ye1107 <jackhank1107@gmail.com>
2023-10-18 11:22:16 +0800

    frist commit

---
+++ database/migrations/2023_10_11_061527_create_usernames_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsernamesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usernames', function (Blueprint $table) {
+            $table->id();
+            $table->string('number');
+            $table->string('name');
+            $table->string('photo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usernames');
+    }
+}
