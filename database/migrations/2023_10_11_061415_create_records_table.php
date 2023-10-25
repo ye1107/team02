2751fd70bdebb912ec5d6f0723aa5aea7295ec98
ye1107 <jackhank1107@gmail.com>
2023-10-25 11:48:21 +0800

    增加關聯式表格欄位定義

--- database/migrations/2023_10_11_061415_create_records_table.php
+++ database/migrations/2023_10_11_061415_create_records_table.php
@@ -15,8 +15,16 @@ class CreateRecordsTable extends Migration
     {
         Schema::create('records', function (Blueprint $table) {
             $table->id()->comment('編號(主鍵)');
-            $table->tinyInteger('uid')->unsigned()->comment('救生員編號(外部鍵2)');
-            $table->tinyInteger('kid')->unsigned()->comment('鑰匙編號(外部鍵1)');
+            $table->foreignId('uid')->comment('救生員編號(外部鍵2)');
+            $table->foreign('uid')
+            ->references('id')
+            ->on('usernames')
+            ->onDelete('cascade');
+            $table->foreignId('kid')->comment('鑰匙編號(外部鍵1)');
+            $table->foreign('kid')
+            ->references('id')
+            ->on('keys')
+            ->onDelete('cascade');
             $table->date('lend_date')->comment('借用日期');
             $table->time('lend_time')->comment('借用時間');
             $table->date('back_date')->comment('歸還日期');
